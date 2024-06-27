4d5d9f8d67fddd8ce881e6cb9ed8cdc434733d9b
stevenbachimont <stevenbachimont@gmail.com>
2024-06-27 09:34:07 +0200

    first pish

---
+++ script.js
@@ -0,0 +1,48 @@
+//var myPolygon;
+function initialize() {
+  // Map Center
+  var myLatLng = new google.maps.LatLng(47.20978,-1.56633);
+  // General Options
+  var mapOptions = {
+    zoom: 12,
+    center: myLatLng,
+    mapTypeId: google.maps.MapTypeId.RoadMap
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+  // Polygon Coordinates
+  var triangleCoords = [
+    new google.maps.LatLng(47.26013,-1.55809), new google.maps.LatLng(47.19065,-1.6628), new google.maps.LatLng(47.19461,-1.44479)
+  ];
+  // Styling & Controls
+  myPolygon = new google.maps.Polygon({
+    paths: triangleCoords,
+    draggable: true, // turn off if it gets annoying
+    editable: true,
+    strokeColor: '#FF0000',
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: '#FF0000',
+    fillOpacity: 0.35
+  });
+
+  myPolygon.setMap(map);
+  //google.maps.event.addListener(myPolygon, "dragend", getPolygonCoords);
+  google.maps.event.addListener(myPolygon.getPath(), "insert_at", getPolygonCoords);
+  //google.maps.event.addListener(myPolygon.getPath(), "remove_at", getPolygonCoords);
+  google.maps.event.addListener(myPolygon.getPath(), "set_at", getPolygonCoords);
+}
+
+//Display Coordinates below map
+function getPolygonCoords() {
+  var len = myPolygon.getPath().getLength();
+  var htmlStr = "";
+  for (var i = 0; i < len; i++) {
+    htmlStr += "[" + myPolygon.getPath().getAt(i).toUrlValue(5) + "], ";
+    //Use this one instead if you want to get rid of the wrap > new google.maps.LatLng(),
+    //htmlStr += "" + myPolygon.getPath().getAt(i).toUrlValue(5);
+  }
+  document.getElementById('info').innerHTML = htmlStr;
+}
+function copyToClipboard(text) {
+  window.prompt("Copy to clipboard: Ctrl+C, Enter", text);
+}
